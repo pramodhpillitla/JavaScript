86c4f64963f8d5dc24572e37d33df52464ccdbd9
Pramodh Pillitla <pramodhpillitla@gmail.com>
2026-06-15 11:43:16 +0000

    Enhance objects.js with examples of object merging and nested objects

--- Basics2/objects.js
+++ Basics2/objects.js
@@ -25,6 +25,37 @@ console.log(obj[sym2]); // Output: "symbol 2"
 
 //Object.freeze() can be used to make an object immutable, meaning that its properties cannot be changed, added, or removed. This is useful for creating constants or ensuring that an object remains unchanged throughout the application.
 
+console.log(Object.keys(person))
+console.log(Object.values(person))
+console.log(Object.entries(person))
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 3, c: 4 };
+
+const obj3 = Object.assign({}, obj1, obj2);
+
+console.log(obj3) // Output: { a: 1, b: 3, c: 4 }
+
+const obj4 = {...obj1, ...obj2}
+console.log(obj4) // Output: { a: 1, b: 3, c: 4 } // these are two methods of merging two objects. The Object.assign() method copies the properties of one or more source objects to a target object, while the spread operator (...) creates a new object by copying the properties of the source objects. Both methods will overwrite properties with the same key, with the last source object taking precedence.
+
+
+//nested objects
+
+const person2 = {
+    name: "Alice",
+    age: 25,
+    city: "Los Angeles",
+    address: {
+        street: "123 Main St",
+        zip: "90001"
+    }
+};
+
+console.log(person2.address.street); // Output: "123 Main St"
+console.log(person2["address"]["zip"]); // Output: "90001"  
+
+
 //2. constructor function 
 function Person(name, age, city) {
     this.name = name;
